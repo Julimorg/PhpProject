b4281ceef15a4f28836c18e83f4fb253db4e7c5e
Julimorg <kienphongtran2003@gmail.com>
2024-02-18 18:37:04 +0700

    php

--- script/homepage.js
+++ script/homepage.js
@@ -30,22 +30,43 @@ slider.addEventListener('mousemove', (e) => {
    const walk = (x - startX) * 2;
    slider.scrollLeft = scrollLeft - walk;
 });
-// Scroller 
-// const scrollers = document.querySelectorAll(".slider_item");   
-// if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches)
-// {
-//     addAnimation();
-// }
-// function addAnimation() 
-// {
-//    scrollers.forEach((scroller)=>{
-//     scroller.setAttribute("data-animated",true);
-//     const courseItem = scroller.querySelector('.course_item');
-//     const scrollerContent = Array.from(courseItem.children);
-//     scrollerContent.forEach(item =>{
-//         const duplicatedItem  = item.cloneNode(true);
-//         duplicatedItem.setAttribute("aria-hidden",true);
-//         courseItem.appendChild(duplicatedItem);
-//     })
-//    })
-// }
+// Swiper slider changes
+let swiper = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    swiper.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
